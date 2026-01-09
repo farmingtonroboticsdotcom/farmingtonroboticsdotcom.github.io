1bb283e1b84a2595b90557a62e9215af185714dc
Owen <189063419+cascaade@users.noreply.github.com>
2026-01-08 19:49:53 -0500

    move stuff, delete stuff, and add workflow

--- server/public/js/login.js
+++
@@ -1,3 +0,0 @@
-// at some point in the future, we should migrate to a google app so that team members can log in with their google accounts directly to access the console
-
-const button = document.getElementById('loading');
